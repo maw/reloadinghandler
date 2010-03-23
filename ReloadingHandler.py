cb06a35ef6c4d74bb7ff9440597ed5ef19b2fe0b
Michael Wolf <maw@pobox.com>
2010-03-23 08:12:47 -0600

    Initial commit.  Add the ReloadingHandler.

---
+++ ReloadingHandler.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import SimpleHTTPServer
+import sys
+
+real_module = None
+
+class reloading_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    __module = None
+    __barf_on_reload_error = True
+    
+    def __reload(self):
+        # It probably isn't necessary to wrap this in try ... except,
+        # because we've already tried once.  Also, SimpleHTTPServer is robust
+        # against exceptions inside its handlers.
+        #
+        # XXX: Is there a need for the following?
+        # for key in sys.modules.keys():
+        #     reload(sys.modules[key])
+        reload(self.__module)
+        
+    # Other methods, as necessary, will look like this.
+    def do_GET(self):
+        self.__reload()
+        self.__module.do_GET(self)
+        
+    def __init__(self, request, client_address, server):
+        try:
+            self.__module = real_module
+            reload(self.__module)
+        except TypeError, te:
+            print "Failed to load module ", `self.__module`, ": ", `te`
+            # XXX: Anything possible other than just bailing here?
+            # XXX: Should we bail at all?
+            sys.exit(1)
+            pass
+        
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+        pass
+    
+
+def handler():
+    return reloading_handler
