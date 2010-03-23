f277122b03ff461823c1e6655ca717736f7df333
Michael Wolf <maw@pobox.com>
2010-03-23 08:13:46 -0600

    Add a sample handler and small program that shows how to use the reloading handler.

---
+++ MyHandler.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import SimpleHTTPServer
+import sys
+from urlparse import urlparse
+
+def gravy():
+    return 'hello dude'
+
+def do_GET(real_handler):
+    print "In the real do_GET"
+    real_handler.send_response(200)
+    real_handler.send_header('Content-type', 'text/html')
+    real_handler.end_headers()
+    real_handler.wfile.write(gravy())
+    return
+
+
+class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+        pass
+    def do_GET(self):
+        do_GET(self)
+    pass
---
+++ simple.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import BaseHTTPServer
+import SimpleHTTPServer
+import SocketServer
+import socket
+import sys
+
+import MyHandler
+import ReloadingHandler
+
+def main(args):
+    PORT = 8000
+    
+    ReloadingHandler.real_module = MyHandler
+    
+    SocketServer.ThreadingTCPServer.allow_reuse_address = True
+    
+    ## When you're developing and testing
+    httpd = SocketServer.ThreadingTCPServer(('localhost', PORT),
+                                            ReloadingHandler.handler())
+    # For production
+    #httpd = SocketServer.ThreadingTCPServer(('localhost', PORT),
+    #                                        MyHandler.MyHandler)
+    
+    httpd.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    httpd.serve_forever()
+    
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
+    pass
+
