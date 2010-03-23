5b94c5fa6bb1de3381cfa1d015bd6fd0e41c9461
Michael Wolf <maw@pobox.com>
2010-03-23 12:40:09 -0600

    Adapt the reloading handler to deal with pure class-based reloaded handlers.

--- ReloadingHandler.py
+++ ReloadingHandler.py
@@ -8,8 +8,11 @@ real_module = None
 class reloading_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     __module = None
     __barf_on_reload_error = True
+    __real_handler_class = None
+    __real_handler = None
     
     def __reload(self):
+        """Reload the module which contains your handler."""
         # It probably isn't necessary to wrap this in try ... except,
         # because we've already tried once.  Also, SimpleHTTPServer is robust
         # against exceptions inside its handlers.
@@ -22,14 +25,18 @@ class reloading_handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     
     # Other methods, as necessary, will look much like this one.
     def do_GET(self):
+        """GET"""
         self.__reload()
-        self.__module.do_GET(self)
+        self.__real_handler.do_GET(up=self)
         pass
     
     def __init__(self, request, client_address, server):
         try:
             self.__module = real_module
             reload(self.__module)
+            self.__real_handler_class = self.__module.handler()
+            self.__real_handler = self.__real_handler_class(request, client_address, server, fake=True)
+            
         except TypeError, te:
             print "Failed to load module ", `self.__module`, ": ", `te`
             # XXX: Anything possible other than just bailing here?
